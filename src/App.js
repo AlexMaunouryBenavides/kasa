b1f53146e0588eafa299bcdd1e8c401c6ab1dfee
alexandremaunoury <alexandremaunoury@yahoo.fr>
2022-12-28 14:07:16 +0100

    Initialize project using Create React App Création du repot

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
-
+import {Routes, Route} from 'react-router-dom'
+import Home from './Pages/Home'
+import Logement from './Pages/logement'
+import Lost from './Pages/404'
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/Logement" element={<Logement />} />
+        <Route path="/404" element={<Lost />} />
+      </Routes>
     </div>
   );
 }
---
+++ src/Pages/404.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Lost () {
+    return (
+        <div>
+            <h1>Page 404</h1>
+        </div>
+    )
+}
